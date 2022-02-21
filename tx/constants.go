7af34a25a81d22a3afb347e96cdb02a98742bb35
Mojtaba <mojtaba.esk@gmail.com>
2022-02-21 17:48:11 +0100

    Action messages updated to the current SDKs change

--- tx/constants.go
+++ tx/constants.go
@@ -9,13 +9,13 @@ const (
 )
 
 const (
-	ACTION_CREATE_VALIDATOR          = "create_validator"
-	ACTION_SEND                      = "send"
-	ACTION_DELEGATE                  = "delegate"
-	ACTION_BEGIN_REDELEGATE          = "begin_redelegate"
-	ACTION_BEGIN_UNBONDING           = "begin_unbonding"
-	ACTION_WITHDRAW_DELEGATOR_REWARD = "withdraw_delegator_reward"
-	ACTION_SUBMIT_PROPOSAL           = "submit_proposal"
-	ACTION_VOTE                      = "vote"
-	ACTION_UNJAIL                    = "unjail"
+	ACTION_CREATE_VALIDATOR          = "/cosmos.staking.v1beta1.MsgCreateValidator"              // "create_validator"
+	ACTION_SEND                      = "/cosmos.bank.v1beta1.MsgSend"                            // "send"
+	ACTION_DELEGATE                  = "/cosmos.staking.v1beta1.MsgDelegate"                     // "delegate"
+	ACTION_BEGIN_REDELEGATE          = "/cosmos.staking.v1beta1.MsgBeginRedelegate"              // "begin_redelegate"
+	ACTION_BEGIN_UNBONDING           = "/cosmos.staking.v1beta1.MsgUndelegate"                   // "begin_unbonding"
+	ACTION_WITHDRAW_DELEGATOR_REWARD = "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward" // "withdraw_delegator_reward"
+	ACTION_SUBMIT_PROPOSAL           = "/cosmos.gov.v1beta1.MsgSubmitProposal"                   // "submit_proposal"
+	ACTION_VOTE                      = "/cosmos.gov.v1beta1.MsgVote"                             // "vote"
+	ACTION_UNJAIL                    = "/cosmos.slashing.v1beta1.MsgUnjail"                      // "unjail"
 )
